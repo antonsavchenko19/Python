fb6d2046098c196a311023645355e9d366893246
John Doe <johndoe@example.com>
2024-03-30 10:52:36 +0300

    add LAB6

---
+++ LAB6/1.2.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(120)
+turtle.color("green")
+for i in range(60):
+ turtle.circle(3*i)
+ turtle.left(70)
---
+++ LAB6/1.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(2)
+turtle.color("blue")
+for i in range(18):
+ turtle.forward(150)
+ turtle.left(103)
