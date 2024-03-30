2137ec4595d8d88425ae7f8f8e7c819a6520ae7b
John Doe <johndoe@example.com>
2024-03-30 11:03:17 +0300

    add LAB7

---
+++ LAB7/1.py
@@ -0,0 +1,16 @@
+guess = int(input("Введите ваше число: "))
+target = 100
+
+difference = guess - target
+
+if difference <= 2 and difference >= -2:
+
+    print("Горячо!")
+
+elif difference <= 4 and difference >= 3:
+
+    print("Тепло!")
+
+else:
+
+    print("Холодно!")
