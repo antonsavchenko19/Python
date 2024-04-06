ffbb983419fd55be115a163ae62931c06f823ab0
John Doe <johndoe@example.com>
2024-04-06 10:27:38 +0300

    restore

--- LAB 08/1.py
+++ LAB 08/1.py
@@ -3,10 +3,16 @@ import random
 print("Добро пожаловать в игру 'Камень, ножницы, бумага, ящерица, Спок'!")
 playerScore = 0
 botScore = 0
+rounds_played = 0
 valid_choices = ["камень", "ножницы", "бумага", "ящерица", "спок"]
 
-for i in range(5):
-    answer = input("Что выберешь?\n").lower()
+while rounds_played < 5:
+    rounds_played += 1
+    
+    print(f"\nРаунд {rounds_played}")
+    answer = input("Что выберешь? (Введите номер варианта)\n1. Камень\n2. Ножницы\n3. Бумага\n4. Ящерица\n5. Спок\n")
+    answer = valid_choices[int(answer) - 1]
+
     if answer in valid_choices:
         botAnswer = random.choice(valid_choices)
         print(f"А я выберу {botAnswer}")
